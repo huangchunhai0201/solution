579da524f32c3ef7b7e4414b28feecc37337d5d9
huangchunhai <huangchunhai0201@163.com>
2017-01-17 14:22:15 +0800

    hch

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var userDao = require('../dao/userDao');
+
 var router = express.Router();
 
 /* GET home page. */
+router.get('/addUser', function(req, res, next) {
+  console.log('sb')
+  userDao.add(req, res, next);
+});
 router.get('/', function(req, res, next) {
+  console.log(2);
   res.render('index', { title: 'hello solution' });
 });
 
