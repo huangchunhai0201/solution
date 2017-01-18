d6119b67515bedfe2263eccd75a1a9401e589071
huangchunhai <huangchunhai0201@163.com>
2017-01-18 16:45:26 +0800

    Signed-off-by: huangchunhai <huangchunhai0201@163.com>

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request= require('request');
+
 var router = express.Router();
 
 /* GET home page. */
@@ -7,6 +9,21 @@ var router = express.Router();
 
 
 // });
+router.get('/test', function(req, res, next) {
+  /*正式数据*/
+  request('http://localhost:8080/rank/json/rank/getAccounts?departmentId=-10',function(error,response,body){
+    /*判断请求是否成功*/
+    if (!error && response.statusCode == 200) {
+      /*把字符串转换为json*/
+      var data=JSON.parse(body);
+      /*渲染模板*/
+      data.title = 'test';
+      console.log(data.result);
+      res.render('test', data);
+    }
+  });
+});
+
 router.get('/', function(req, res, next) {
   //数据
   var data = {
@@ -26,5 +43,6 @@ router.get('/', function(req, res, next) {
   //渲染模板
   res.render('index', data);
 });
+
 
 module.exports = router;
