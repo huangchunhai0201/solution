650d883e6a3a2b3483cfdd05d790ff145b0e8058
huangchunhai <huangchunhai0201@163.com>
2017-01-18 14:27:59 +0800

    hch

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var template=require('art-template');
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
+
+
+template.config('base','');
+template.config('extname','.html');
+app.engine('.html',template.__express);
+app.set('view engine','html');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express1',indexO:{"id": 1} });
+
+
+// });
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express1' });
+  //数据
+  var data = {
+    title: '国内要闻1',
+    time: (new Date).toString(),
+    list: [
+      {
+        id: '1',
+        name: '张三'
+      },
+      {
+        id: '2',
+        name: '李四'
+      }
+    ]
+  };
+  //渲染模板
+  res.render('index', data);
 });
 
 module.exports = router;
