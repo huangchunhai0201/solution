ae2e9649f5dfff658c2025229cb064793a889256
huangchunhai <huangchunhai0201@163.com>
2017-02-03 17:25:33 +0800

    Signed-off-by: huangchunhai <huangchunhai0201@163.com>

---
+++ public/js/p_Badge.js
@@ -0,0 +1,61 @@
+(function () {
+    Pithy.p_Badge = p_Badge;
+    p_Badge.renders = {
+        'default': '<div class="p_Badge">${label}<span>${count}</span></div>'
+    };
+    function p_Badge(info) {
+        this._info = info;
+        this._agent = this;
+    }
+
+    p_Badge.prototype = {
+        getRender: function (render) {
+            return p_Badge.renders[render];
+        },
+        buildRender: function (render) {
+            if (!render) {
+                render = "default";
+            }
+            var html = this.getRender(render);
+
+            return juicer(html, this._info);
+        },
+        build: function (jParentDiv) {
+            var self = this;
+            if (!jParentDiv) {
+                jParentDiv = $(".container");
+            }
+            this._jParentDiv = $(jParentDiv);
+            this._jWrap = this._jParentDiv;
+            var render = this.buildRender(this._info.render);
+            this._jControl = $(render);
+            this._jWrap.html(this._jControl);
+
+            this._jControl.css({
+                "cursor": "pointer"
+            });
+            this._jControl.hover(function () {
+                self._jControl.css({
+                    color: 'black'
+                });
+            }, function () {
+                self._jControl.css({
+                    color: '#FFFFFF'
+                });
+            });
+            var p_modal = new Pithy.p_Modal({
+                'title': '消息列表',
+                'style': {
+                    'width': '40%',
+                    'height': '60vh'
+                },
+                'content': '<p style="padding-left: 12px;">系统开发中。。。</p>'
+            });
+            p_modal.build('body');
+            this._jControl.on('click', function () {
+                p_modal.open();
+            });
+
+        }
+    };
+})();
---
+++ public/js/p_Modal.js
@@ -0,0 +1,64 @@
+(function () {
+    Pithy.p_Modal = p_Modal;
+    p_Modal.renders = {
+        "default": "<div class='p_modal'>" +
+        "<div class='p-modal-content'>" +
+        "<span class='p-modal-close'><i class='icon anticon icon-close'></i></span>" +
+        "<div class='p-modal-header'>" +
+        "<span class='p-modal-title'>${title}</span>" +
+        "</div>" +
+        "<div class='p-modal-body'>$${content}</div>" +
+        "<div class='p-modal-footer'>" +
+        "<button type='button' class='p_button'>关闭</button>" +
+        "</div>" +
+        "</div>" +
+        "</div>"
+    };
+
+    function p_Modal(info) {
+        this._info = info;
+        this._agent = this;
+    }
+
+    p_Modal.prototype = {
+        getRender: function (render) {
+            return p_Modal.renders[render];
+        },
+        buildRender: function (render) {
+            if (!render) {
+                render = 'default';
+            }
+            var html = this.getRender(render);
+            if (!this._info.content) {
+                this._info.content = 'loading...';
+            }
+            return juicer(html, this._info);
+        },
+        build: function (jParentDiv) {
+            var self = this;
+            this._jParentDiv = $(jParentDiv);
+            this._jWrap = $(jParentDiv);
+
+            var renderHtml = this.buildRender(this._info.render);
+            this._jControl = $(renderHtml);
+
+            this._jWrap.append(this._jControl);
+
+            var defaultPro = {
+                'width': '70%',
+                'height': '60vh'
+            };
+            if (this._info.style) {
+                $.extend(defaultPro, this._info.style);
+            }
+            this._jControl.css(defaultPro);
+
+            this._jControl.find('.p-modal-close').on('click', function () {
+                self._jControl.hide();
+            });
+        },
+        open: function () {
+            this._jControl.show();
+        }
+    };
+})();
